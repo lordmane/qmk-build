3fb9034849849e37297d7a02068fc22a13727075
Oscar Prieto <oprieto@externos.itnow.es>
2018-08-30 16:03:09 +0200

    now we can flash

--- build.sh
+++ build.sh
@@ -3,14 +3,16 @@
 REPO=https://github.com/oprietop/qmk_firmware.git
 KEYB=xd75:default
 
-docker build . -t oprietop/qmk-flash
+#docker build . -t oprietop/qmk-flash
 
 if [ ! -d qmk_firmware ] ; then
     git clone $REPO
-else
-    cd qmk_firmware
-    git pull
 fi
 
 cd qmk_firmware
-docker run -e keyboard=${1:-xd75:default}  --rm -v $('pwd'):/qmk:rw -v /dev/bus/usb:/dev/bus/usb oprietop/qmk-flash
+git remote add upstream https://github.com/qmk/qmk_firmware.git
+git pull
+git fetch upstream
+git merge upstream/master --no-edit
+
+docker run --privileged -e keyboard=${1:-xd75:default}  --rm -v $('pwd'):/qmk:rw --mount type=bind,source=/dev,target=/dev oprietop/qmk-flash
