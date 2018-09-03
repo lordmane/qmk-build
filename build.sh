ddd7e657917d69222db77267391e6d660651c3b9
Oscar Prieto <oprieto@externos.itnow.es>
2018-09-03 09:57:12 +0200

    more cleaning

--- build.sh
+++ build.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
-
 REPO=https://github.com/oprietop/qmk_firmware.git
-KEYB=xd75:default
-
-#docker build . -t oprietop/qmk-flash
 
 if [ ! -d qmk_firmware ] ; then
     git clone $REPO
 fi
 
 cd qmk_firmware
+git pull
 git remote add upstream https://github.com/qmk/qmk_firmware.git
 git checkout master
-git pull
 git pull upstream master
 
 docker run --privileged -e keyboard=${1:-xd75:default}  --rm -v $('pwd'):/qmk:rw --mount type=bind,source=/dev,target=/dev oprietop/qmk-flash
